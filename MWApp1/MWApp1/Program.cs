ecbb5a4493cb39ad31296ea12c2fac6c6660534d
MichalWluka <michal.wluka@gmail.com>
2023-03-13 17:16:12 +0100

    zadanie domowe dzień 4

--- MWApp1/MWApp1/Program.cs
+++ MWApp1/MWApp1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Podaj Imię?
+var imię = "Ewa";
+// Podaj Wiek?
+var wiek = 33;
+// Podaj płeć, k lub m 
+var płeć = 'k';
+
+
+if (imię == "Ewa" && płeć == 'k' && wiek == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+if(płeć == 'k' && wiek < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+if (płeć == 'm' && wiek >= 18)
+{
+    Console.WriteLine("Pełnoletni Mężczyzna");
+}
+else if (wiek < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
