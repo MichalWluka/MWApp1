509904f604b4de0b660106a0b67d35f17b0c93fd
MichalWluka <michal.wluka@gmail.com>
2023-03-21 11:03:46 +0100

    zadanie domowe dzień 5

--- MWApp1/MWApp1/Program.cs
+++ MWApp1/MWApp1/Program.cs
@@ -1,25 +1,79 @@
 ﻿
-// Podaj Imię?
-var imię = "Ewa";
-// Podaj Wiek?
-var wiek = 33;
-// Podaj płeć, k lub m 
-var płeć = 'k';
+int liczba = 15555425; // podaj liczbę
+string liczbaString = liczba.ToString();
+char[] litery = liczbaString.ToArray();
+
+Console.WriteLine("Podana liczba wynosi: " + liczbaString);
+
+int licznik0 = 0;
+int licznik1 = 0;
+int licznik2 = 0;
+int licznik3 = 0;
+int licznik4 = 0;
+int licznik5 = 0;
+int licznik6 = 0;
+int licznik7 = 0;
+int licznik8 = 0;
+int licznik9 = 0;
 
 
-if (imię == "Ewa" && płeć == 'k' && wiek == 33)
+foreach (char i in litery)
 {
-    Console.WriteLine("Ewa, lat 33");
+    if (i == '0')
+    {
+        licznik0++;
+    }
+    else if (i == '1')
+    {
+        licznik1++;
+    }
+    else if (i == '2')
+    {
+        licznik2++;
+    }
+    else if (i == '3')
+    {
+        licznik3++;
+    }
+    else if (i == '4')
+    {
+        licznik4++;
+    }
+    else if (i == '5')
+    {
+        licznik5++;
+    }
+    else if (i == '6')
+    {
+        licznik6++;
+    }
+    else if (i == '7')
+    {
+        licznik7++;
+    }
+    else if (i == '8')
+    {
+        licznik8++;
+    }
+    else if (i == '9')
+    {
+        licznik9++;
+    }
 }
-if(płeć == 'k' && wiek < 30)
+
+List<int> liczniki = new List<int>();
+liczniki.Add(licznik0);
+liczniki.Add(licznik1);
+liczniki.Add(licznik2);
+liczniki.Add(licznik3);
+liczniki.Add(licznik4);
+liczniki.Add(licznik5);
+liczniki.Add(licznik6);
+liczniki.Add(licznik7);
+liczniki.Add(licznik8);
+liczniki.Add(licznik9);
+
+for (int n = 0; n < liczniki.Count; n++) 
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
-if (płeć == 'm' && wiek >= 18)
-{
-    Console.WriteLine("Pełnoletni Mężczyzna");
-}
-else if (wiek < 18)
-{
-    Console.WriteLine("Niepełnoletni Mężczyzna");
+    Console.WriteLine(n + " => "+ liczniki[n]);
 }
